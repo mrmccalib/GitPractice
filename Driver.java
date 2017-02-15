e2cd28f2959fdd025ea58ee76d6b4de930912eed
mslynch <mslynch95@gmail.com>
2017-02-14 20:13:54 -0600

    Terminal 2 in Step 3: Modified Employee.java

--- Driver.java
+++ Driver.java
@@ -4,9 +4,9 @@ import edu.csbsju.cs.*;
 import static java.lang.System.*;
 
 public class Driver
-{  
+{
    public static void main(String[] args)
-   {  
+   {
       // because of the import statement, we don't have to use
       // edu.csbsju.cs.Employee here
       Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
@@ -18,5 +18,8 @@ public class Driver
       // use java.lang.System.out here
       out.println("name=" + harry.getName() + ",salary=" + harry.getSalary());
    }
-}
 
+   public String toString() {
+       return harry.getName() + ", " + harry.getSalary() + ", " harry.getHireDay();
+   }
+}
